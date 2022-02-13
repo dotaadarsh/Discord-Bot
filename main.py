3434ca4355c99e71a92ccee236cf8ff48daa4d0e
Aadarsh K <71810927+dotaadarsh@users.noreply.github.com>
2022-02-13 11:29:51 +0530

    Using feedparser library

--- main.py
+++ main.py
@@ -1,84 +1,102 @@
-import requests
+import os
 import json
+import asyncio
+import platform
 import discord
 from discord.ext import commands
-from discord.ext import tasks
+import feedparser
+from keep_alive import keep_alive
 from datetime import datetime
-from googleapiclient.discovery import build
 
-client = commands.Bot(command_prefix="==")
-Key = "#"  # Replace with your API key.
-BlogID = "#"  # Replace with your BlogId here.
-Roles = ["ATOM", "BOT"]  # Add your server roles here.
-blog = build("blogger", "v3", developerKey=Key)
-Token ='#' # Replace with Discord Bot Token.
+configfile = json.load(open("config.json", "r", encoding="utf-8"))
+
+Token = configfile["Token"] # Bot token
+
+Bot_Prefix = configfile["Bot_Prefix"] # Bot prefix
+
+Feed_URL = configfile["Feed_URL"] # RSS Feed link
+
+Channel_ID = int(configfile["Channel_ID"]) # Channel id in which you need to send the updates
+
+LogChannel_ID = int(configfile["LogChannel_ID"]) #log of updates  Channel ID
+
+Wait = int(configfile["Wait"]) # Delay of sending the request in seconds
+
+client = commands.Bot(command_prefix=Bot_Prefix)
 
 @client.event
 async def on_ready():
-    print("Bot has started running")
-    await client.change_presence(activity=discord.Game(name="cmd: ==search"))
+    print(f"Bot is ready to be used!")
 
-@client.command()
-async def search(ctx, arg):
-    search_term = str(arg).replace(" ", "%")
-    base_url = "https://www.googleapis.com/blogger/v3/blogs/" + BlogID + "/posts/search"
-    complete_url = base_url + "?q=" + search_term + \
-        "&key=" + Key
-    response = requests.get(complete_url)
-    result = response.json()
+    Feed_channel = client.get_channel(Channel_ID)
+    Log_channel = client.get_channel(LogChannel_ID)
 
-    now = datetime.now()
+    # Online Notice
+    await Log_channel.send(f""" **+ BOT ONLINE!** - {datetime.now()} - Bot Prefix: `{Bot_Prefix}` - Request Send Time: *{Wait}* Seconds """)
 
-    current_time = now.strftime("%H:%M:%S")
+    while True:
+        try:
+            # Feedparser is real helpful here
+            blog_feed = feedparser.parse(Feed_URL)
 
-    embed = discord.Embed(title="List of Search results",
-                          description="Checked on " + f"{current_time}\n", color=0x349bfc)
-    embed.set_author(name="Your Website Name")
-    embed.set_thumbnail(url="https://www.yourwebsite.com/favicon.ico")
-    try:
-        for count, value in enumerate(result["items"]):
-            title = result["items"][count]["title"]
-            url = result["items"][count]["url"]
-            embed.description = embed.description + \
-                f"{count + 1}. [{title}]({url})\n"
-        embed.set_footer(text='This message will be deleted in 60min.')
+            # Get the title of the current video at top
+            TITLE = blog_feed.entries[0].title
 
-        await ctx.send(embed=embed, delete_after=3600.0)
-    except:
-        await ctx.send("There is something wrong with the response.")
+            #  If the current title is same as the last title recieved
+            # (stored in old.txt) btw
+            # This will be skipped and will be tried again in the next round
+            try:
+                with open("old.txt", "r", encoding="utf-8") as old_file:
+                    OLD_TITLE = old_file.read()
+            except:
+                OLD_TITLE = "NO FILE FOUND"
 
-client.recentPosts = None
-client.recentPostsTime = None
-client.recentPostsEdit = None
+            if TITLE == OLD_TITLE:
+                await Log_channel.send(f"""**- Info:** RSS Feed not updated yet.""")
 
+            # If the above check is false
+            else:
+                with open("old.txt", "w", encoding="utf-8") as new_file:
+                    new_file.write(TITLE)
 
-@tasks.loop(seconds=5.0)
-async def fetchUpdates():
-    posts = blog.posts().list(blogId=BlogID).execute()
-    postsList = posts["items"]
-    postTime = postsList[0]["published"]
-    if not client.recentPosts:
-        client.recentPostsTime = postTime
-        client.recentPosts = postsList
-    elif client.recentPostsTime != postTime:
-        titleValue = str(posts["items"][0]["title"])
-        urlValue = str(posts["items"][0]["url"])
+                LINK = blog_feed.entries[0].link
+                PUB_DATE = blog_feed.entries[0].published
+                SUMMARY = blog_feed.entries[0].summary
+                print("-"*25)
+                print("Title:", TITLE)
+                print("Link:", LINK)
+                print("Published Date:", PUB_DATE)
+                print("Summary:", SUMMARY)
 
-        channel = client.get_channel(#)  # Add channel ID
-        embed = discord.Embed(title="New posts available on the blog!",
-                              description=f"[{titleValue}]({urlValue})")
-        channel = client.get_channel(#)  # Add channel ID
-        embed = discord.Embed(title="New posts available on the blog!",
-                              description=f"[{titleValue}]({urlValue})")
-        embed.set_author(name="Your Website")
-        embed.set_thumbnail(url="https://www.yourwebsite.com/favicon.ico")
-        for i in Roles:
-            if i.lower() in postsList[0]["title"].lower():
-                guild = client.guilds[0]
-                await channel.send(discord.utils.get(guild.roles, name=i).mention, delete_after=86400.0)
-                await channel.send(embed=embed, delete_after=86400.0)
-                break
-        client.recentPostsTime = postTime
-        client.recentPosts = postsList
-fetchUpdates.start()
-client.run(Token, bot=True)
+                embed = discord.Embed(
+                    title=f"""{TITLE}""", color=0x00ff00)
+                embed.set_author(name=f"{client.user.name}",
+                                 icon_url=f"{client.user.avatar_url}")
+                embed.add_field(
+                    name="Title", value=f"""{TITLE}""", inline=False)
+                embed.add_field(
+                    name="Link", value=f"""{LINK}""", inline=False)
+                embed.add_field(name="Published on",
+                                value=f"""{PUB_DATE}""", inline=False)
+                embed.add_field(name="Decription",
+                                value=f"""{SUMMARY}""", inline=False)
+                await Feed_channel.send(embed=embed)
+                await Log_channel.send(f"""**+ Success:** Sent *{TITLE}*""")
+
+        except Exception as e:
+            # If this happens....
+            # DAMN! something is really wrong
+            await Log_channel.send(f"""**- Error:** {e}""")
+
+        await asyncio.sleep(Wait)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('^d hello'):
+        await message.channel.send('Hello!')
+
+keep_alive()
+client.run(Token)
